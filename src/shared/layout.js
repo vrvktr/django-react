4d99d7b2aa7b782f57ed253209492d2d7c757dc7
vrvktr <vrk.jun@gmail.com>
2024-01-28 15:03:01 +0530

    1

--- src/shared/layout.js
+++ src/shared/layout.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { createBrowserHistory } from 'history';
 import Header from './header/header';
 
 const Layout = () => {
-    const history = createBrowserHistory()
     return (
         <div className=''>
-            <Header
-                history={history}
-                avatar={"Foo Baar"}
-            />
+            <Header avatar={"Foo Baar"} />
             <div className='py-1'>
                 <Outlet />
             </div>
